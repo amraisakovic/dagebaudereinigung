748c218f09a0abe36dd70c2e5f2e6d650718f79a
Amra Isakovic <amra,isakovic@stu.ssst.edu.ba>
2024-06-27 13:54:36 +0200

    -m "design addition 1"

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Welcome to My Portfolio!');
+    console.log('DA Gebaudereinigung is loaded');
 });
+
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function showNextSlide() {
+    slideIndex = (slideIndex + 1) % totalSlides;
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+setInterval(showNextSlide, 3000);
