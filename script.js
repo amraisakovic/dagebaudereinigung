838feb26dc7a613f0f36cd7551d805147c9bc1e7
Amra Isakovic <amra,isakovic@stu.ssst.edu.ba>
2024-07-10 10:20:07 +0200

    -m "design update"

--- script.js
+++ script.js
@@ -78,3 +78,29 @@ $(document).ready(function(){
     });
 
 });
+
+// Get the popup
+var popup = document.getElementById("impressumPopup");
+
+// Get the button that opens the popup
+var btn = document.getElementById("impressumBtn");
+
+// Get the <span> element that closes the popup
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the popup
+btn.onclick = function() {
+    popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
